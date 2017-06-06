86e10c1fa505a67308203dbf81d44db7c270eadf
ShengJunHu <mr.ajun@foxmail.com>
2017-06-06 10:04:39 +0800

    no message

--- Library/VideoProvider/src/main/java/com/hsj/videoprovider/VideoMainActivity.java
+++ Library/VideoProvider/src/main/java/com/hsj/videoprovider/VideoMainActivity.java
@@ -3,11 +3,11 @@ package com.hsj.videoprovider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class VIderoMainActivity extends AppCompatActivity {
+public class VideoMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_videro_main);
+        setContentView(R.layout.activity_video_main);
     }
 }
