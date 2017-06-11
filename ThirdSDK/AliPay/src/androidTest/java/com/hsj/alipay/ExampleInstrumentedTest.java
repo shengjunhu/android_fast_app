d23d1e85e8add115fa26495d4150ebef4cf77f04
DESKTOP-0CA1SL7\HSJ <mr.ajun@foxmail.com>
2017-06-12 02:04:38 +0800

    no message

--- ThirdSDK/AliPay/src/androidTest/java/com/hsj/alipay/ExampleInstrumentedTest.java
+++ ThirdSDK/AliPay/src/androidTest/java/com/hsj/alipay/ExampleInstrumentedTest.java
@@ -3,10 +3,10 @@ package com.hsj.alipay;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.*;
 
 /**
