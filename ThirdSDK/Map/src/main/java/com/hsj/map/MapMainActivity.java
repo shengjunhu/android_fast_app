5d0db388e08099c7d686eec895a7762f2c6efc85
ShengJunHu <mr.ajun@foxmail.com>
2017-06-12 10:56:39 +0800

    no message

---
+++ ThirdSDK/Map/src/main/java/com/hsj/map/MapMainActivity.java
@@ -0,0 +1,13 @@
+package com.hsj.map;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_main);
+    }
+}
---
+++ ThirdSDK/Pay/src/main/java/com/hsj/pay/PayMainActivity.java
@@ -0,0 +1,13 @@
+package com.hsj.pay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PayMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_main);
+    }
+}
