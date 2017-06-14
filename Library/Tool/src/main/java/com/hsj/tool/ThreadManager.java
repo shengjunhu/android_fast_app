fcd262d83acdd312a22827998af82b9c4783d590
DESKTOP-0CA1SL7\HSJ <mr.ajun@foxmail.com>
2017-06-15 00:32:23 +0800

    no message

--- Library/Tool/src/main/java/com/hsj/tool/ThreadManager.java
+++ Library/Tool/src/main/java/com/hsj/tool/ThreadManager.java
@@ -2,4 +2,21 @@ package com.hsj.tool;
 
 public class ThreadManager {
 
+    private volatile static ThreadManager mThreadManager;
+
+    private ThreadManager(){
+
+    }
+
+    public static  ThreadManager getInstance(){
+        if(mThreadManager==null){
+            synchronized (ThreadManager.class){
+                if (mThreadManager==null){
+                    mThreadManager = new ThreadManager();
+                }
+            }
+        }
+        return mThreadManager;
+    }
+
 }
