66036231e82874914ad43e9121a453de5f9830b4
DESKTOP-0CA1SL7\HSJ <mr.ajun@foxmail.com>
2017-06-19 00:46:30 +0800

    no message

--- Library/Http/src/main/java/com/hsj/http/HttpMainActivity.java
+++ Library/Http/src/main/java/com/hsj/http/HttpMainActivity.java
@@ -9,6 +9,5 @@ public class HttpMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_http_main);
-
     }
 }
