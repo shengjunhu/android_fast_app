95c46356653b07cc5f75300de08caf8789e43292
ShengJunHu <mr.ajun@foxmail.com>
2017-06-19 17:17:22 +0800

    no message

---
+++ Library/DBObject/src/main/java/com/hsj/dbobject/DBObjectActivity.java
@@ -0,0 +1,13 @@
+package com.hsj.dbobject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DBObjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_db_object);
+    }
+}
