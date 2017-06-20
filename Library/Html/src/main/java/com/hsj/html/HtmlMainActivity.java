f379c1025542ab0abeecbf1b9ccbf03d581bf4dd
DESKTOP-0CA1SL7\HSJ <mr.ajun@foxmail.com>
2017-06-20 09:03:50 +0800

    no message

---
+++ Library/Html/src/main/java/com/hsj/html/HtmlMainActivity.java
@@ -0,0 +1,13 @@
+package com.hsj.html;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HtmlMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_html_main);
+    }
+}
