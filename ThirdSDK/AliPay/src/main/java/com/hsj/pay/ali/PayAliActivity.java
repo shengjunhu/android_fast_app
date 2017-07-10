b8f1f62c1a1e884babd8156d8a94fc727b5ce6bb
ShengJunHu <mr.ajun@foxmail.com>
2017-07-10 10:45:44 +0800

    no message

---
+++ ThirdSDK/AliPay/src/main/java/com/hsj/pay/ali/PayAliActivity.java
@@ -0,0 +1,13 @@
+package com.hsj.pay.ali;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PayAliActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_ali);
+    }
+}
---
+++ ThirdSDK/WXPay/src/main/java/com/hsj/pay/wx/PayWxActivity.java
@@ -0,0 +1,13 @@
+package com.hsj.pay.wx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PayWxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_wx);
+    }
+}
