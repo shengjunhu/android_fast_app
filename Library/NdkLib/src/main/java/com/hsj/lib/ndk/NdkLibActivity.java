5bc60b275e99eb8dc63f507b14683084778f3e0d
ShengJunHu <mr.ajun@foxmail.com>
2017-07-13 11:37:51 +0800

    no message

---
+++ Library/NdkLib/src/main/java/com/hsj/lib/ndk/NdkLibActivity.java
@@ -0,0 +1,13 @@
+package com.hsj.lib.ndk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NdkLibActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lib_ndk);
+    }
+}
