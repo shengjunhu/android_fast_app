769e915c72b01077fe5ca0e5c4d863b0147677cc
ShengJunHu <mr.ajun@foxmail.com>
2017-07-14 18:21:34 +0800

    no message

---
+++ Library/Widget/src/main/java/com/hsj/widget/refresh/RefreshLoadLayout.java
@@ -0,0 +1,18 @@
+package com.hsj.widget.refresh;
+
+/**
+ * @Author:HSJ
+ * @E-mail:mr.ajun@foxmail.com
+ * @Date:2017/7/14 18:11
+ * @Class:RefreshLoadLayout
+ * @Description:
+ * 1、下拉刷新：下拉刷新动画
+ * 2、上拉加载布局：上拉刷新动画、文本提示状态(数据加载完毕、没有更多数据、点击加载更多)
+ * 3、加头：支持View、layoutId
+ * 5、加脚：支持View、layoutId
+ * 6、支持RecycleView、LinearLayout、RelativeLayout
+ * 7、当RecycleView无数据，显示无数据占位布局（支持View、layoutId）
+ */
+public class RefreshLoadLayout {
+
+}
