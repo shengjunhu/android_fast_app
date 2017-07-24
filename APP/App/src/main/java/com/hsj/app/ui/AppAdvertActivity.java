908b64d801e237bcb07a88fdfaf74f40c69f789b
ShengJunHu <mr.ajun@foxmail.com>
2017-07-24 21:11:04 +0800

    no message

---
+++ APP/App/src/main/java/com/hsj/app/ui/AppAdvertActivity.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2017.   ShengJunHu
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ ******************************************************************************/
+
+package com.hsj.app.ui;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.hsj.base.app.ui.BaseActivity;
+
+/**
+ * @Author:HSJ
+ * @E-mail:mr.ajun@foxmail.com
+ * @Date:2017/7/24 10:09
+ * @Class:AppAdvertActivity
+ * @Description:广告页
+ */
+public class AppAdvertActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initUI(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
