9d88a0258ef718ff402d5cdc51546648780fb535
DESKTOP-0CA1SL7\HSJ <mr.ajun@foxmail.com>
2017-08-07 00:57:57 +0800

    no message

--- Library/Base/src/main/java/com/hsj/app/base/uitls/SharedPrefer.java
+++ Library/Base/src/main/java/com/hsj/app/base/uitls/SharedPrefer.java
@@ -15,13 +15,15 @@ public class SharedPrefer {
 
     /**
      * 默认sp文件名：
-     * 1、AppInfo:  App相关信息（版本号、该版本号第一次登陆）
-     * 2、UserInfo: 用户信息(账号、密码、token、设备ID)
+     * 1、AppInfo:    App相关信息（版本号、该版本号第一次登陆）
+     * 2、UserInfo:   用户信息(账号、密码、token、设备ID)
+     * 3、ActionInfo: 上一次程序未执行完的操作标志，继续操作
      */
     private String fileName = "config";
 
     /**
      * 动态配置sp文件名
+     *
      * @param fileName
      */
     public void setFileName(@NonNull String fileName) {
@@ -29,50 +31,73 @@ public class SharedPrefer {
     }
 
     /**
-     * 可以set ：Int String Boolean类型数据
+     * 可以set ：int 类型数据
      *
      * @param key
      * @param value
      */
-    public void set(Context context, String key, Object value) {
+    public void setInt(Context context, @NonNull String key, int value) {
         SharedPreferences sp = context.getSharedPreferences(fileName, Context.MODE_PRIVATE);
         SharedPreferences.Editor edit = sp.edit();
-        if (value instanceof String) {
-            edit.putString(key, (String) value);
-        } else if (value instanceof Integer) {
-            edit.putInt(key, (int) value);
-        } else if (value instanceof Boolean) {
-            edit.putBoolean(key, (boolean) value);
-        }
+        edit.putInt(key, value);
+        edit.apply();
+    }
+
+    /**
+     * 可以set ：String类型数据
+     *
+     * @param key
+     * @param value
+     */
+    public void setString(Context context, @NonNull String key, String value) {
+        SharedPreferences sp = context.getSharedPreferences(fileName, Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = sp.edit();
+        edit.putString(key, value);
+        edit.apply();
+    }
+
+    /**
+     * 可以set ：boolean类型数据
+     *
+     * @param key
+     * @param value
+     */
+    public void setBoolean(Context context, @NonNull String key, boolean value) {
+        SharedPreferences sp = context.getSharedPreferences(fileName, Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = sp.edit();
+        edit.putBoolean(key, value);
         edit.apply();
     }
 
     /**
      * 获取字符串
+     *
      * @param key
      * @return
      */
-    public String getString(Context context, String key) {
+    public String getString(Context context, @NonNull String key) {
         SharedPreferences sp = context.getSharedPreferences(fileName, Context.MODE_PRIVATE);
         return sp.getString(key, "");
     }
 
     /**
      * 获取整数
+     *
      * @param key
      * @return
      */
-    public int getInt(Context context, String key) {
+    public int getInt(Context context, @NonNull String key) {
         SharedPreferences sp = context.getSharedPreferences(fileName, Context.MODE_PRIVATE);
-        return sp.getInt(key, 0);
+        return sp.getInt(key, -1);
     }
 
     /**
      * 获取布尔
+     *
      * @param key
      * @return
      */
-    public boolean getBoolean(Context context, String key) {
+    public boolean getBoolean(Context context, @NonNull String key) {
         SharedPreferences sp = context.getSharedPreferences(fileName, Context.MODE_PRIVATE);
         return sp.getBoolean(key, false);
     }
