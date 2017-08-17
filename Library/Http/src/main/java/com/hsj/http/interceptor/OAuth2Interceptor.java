58ae7efb2f7fae6b452f6040ae35ae2c4779e247
ShengJunHu <mr.ajun@foxmail.com>
2017-08-17 17:09:29 +0800

    no message

--- Library/Http/src/main/java/com/hsj/http/interceptor/OAuth2Interceptor.java
+++ Library/Http/src/main/java/com/hsj/http/interceptor/OAuth2Interceptor.java
@@ -1,7 +1,11 @@
 package com.hsj.http.interceptor;
 
+import android.util.Log;
+
 import java.io.IOException;
+
 import okhttp3.Interceptor;
+import okhttp3.Request;
 import okhttp3.Response;
 
 /**
@@ -11,10 +15,30 @@ import okhttp3.Response;
  * @Class:OAuth2Interceptor
  * @Description:基于OAuth2网络请求，拦截本次请求先获取token再执行本次请求
  */
-public class OAuth2Interceptor implements Interceptor{
+public class OAuth2Interceptor implements Interceptor {
+
+    private String TAG = this.getClass().getSimpleName();
 
     @Override
     public Response intercept(Chain chain) throws IOException {
-        return null;
+
+        //执行token请求
+        String accessToken = "";
+
+        Request request = chain.request();
+        Request.Builder builder = request.newBuilder();
+        builder.header("Authorization","");
+        builder.header("deviceId", "");
+        builder.header("secToken", accessToken);
+        builder.header("loginToken", "");
+
+        //---------请求之前-----
+        Log.d(TAG, "app interceptor:begin");
+
+        Response response = chain.proceed(request);
+
+        Log.d(TAG, "app interceptor:end");
+        //---------请求之后------------
+        return response;
     }
 }
--- Library/Http/src/main/java/com/hsj/http/oauth2/HttpManagers.java
+++ Library/Http/src/main/java/com/hsj/http/oauth2/HttpManagers.java
@@ -1,11 +1,5 @@
 package com.hsj.http.oauth2;
 
-import android.util.Log;
-import java.io.IOException;
-import okhttp3.Interceptor;
-import okhttp3.Request;
-import okhttp3.Response;
-
 /**
  * @Author:HSJ
  * @E-mail:mr.ajun@foxmail.com
@@ -16,26 +10,5 @@ import okhttp3.Response;
 public class HttpManagers {
 
     private String TAG = this.getClass().getSimpleName();
-
-    /**
-     * 应用拦截器
-     */
-    Interceptor appInterceptor = new Interceptor() {
-        @Override
-        public Response intercept(Chain chain) throws IOException {
-            Request request = chain.request();
-            Request.Builder builder = request.newBuilder();
-            builder.header("OAUTH2","");
-
-            //---------请求之前-----
-            Log.d(TAG,"app interceptor:begin");
-            Response response = chain.proceed(request);
-            Log.d(TAG,"app interceptor:end");
-            //---------请求之后------------
-            return response;
-        }
-
-    };
-
 
 }
