118611ba14698ec26a6214da95b7c202fd846454
hushengjun <mr.ajun@foxmail.com>
2017-10-25 21:02:24 +0800

    chang zbar to zxing

---
+++ Library/QRProvider/src/main/java/com/hsj/provider/qr/QRCreateProvder.java
@@ -0,0 +1,13 @@
+package com.hsj.provider.qr;
+
+/**
+ * @Company:南京荣之誉信息科技有限公司
+ * @Author:HSJ
+ * @E-mail:mr.ajun@foxmail.com
+ * @Date:2017/10/25/20:23
+ * @Version:XBS V2.0
+ * @Class:QRCreateProvder
+ * @Description:
+ */
+public class QRCreateProvder {
+}
---
+++ Library/QRProvider/src/main/java/com/hsj/provider/qr/view/QRView.java
@@ -0,0 +1,13 @@
+package com.hsj.provider.qr.view;
+
+/**
+ * @Company:南京荣之誉信息科技有限公司
+ * @Author:HSJ
+ * @E-mail:mr.ajun@foxmail.com
+ * @Date:2017/10/25/20:22
+ * @Version:XBS V2.0
+ * @Class:QRView
+ * @Description:
+ */
+public class QRView {
+}
