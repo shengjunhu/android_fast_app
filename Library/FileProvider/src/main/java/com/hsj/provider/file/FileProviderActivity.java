6f9122bfc6bb6d9fb4b774a9b4ab2cf35befe8dd
hushengjun <mr.ajun@foxmail.com>
2017-12-05 12:18:57 +0800

    no message

--- Library/FileProvider/src/main/java/com/hsj/provider/file/FileProviderActivity.java
+++ Library/FileProvider/src/main/java/com/hsj/provider/file/FileProviderActivity.java
@@ -1,9 +1,7 @@
 package com.hsj.provider.file;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.hsj.fileprovider.R;
+import android.support.v7.app.AppCompatActivity;
 
 public class FileProviderActivity extends AppCompatActivity {
 
