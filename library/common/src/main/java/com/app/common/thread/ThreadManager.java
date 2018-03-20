c55d41ea5a4c659f17ad006064f52c52f77cfe88
hushengjun <mr.ajun@foxmail.com>
2018-03-20 09:52:20 +0800

    add threadmanager

--- library/common/src/main/java/com/app/common/thread/ThreadManager.java
+++ library/common/src/main/java/com/app/common/thread/ThreadManager.java
@@ -16,13 +16,10 @@
 
 package com.app.common.thread;
 
-import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Looper;
 
-import java.lang.reflect.Field;
 import java.util.concurrent.Executor;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.RejectedExecutionException;
@@ -30,16 +27,19 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
- * 腾讯线程池
+ * @Author:HSJ
+ * @E-mail:mr.ajun@foxmail.com
+ * @Date:2017-06-15 23:50
+ * @Class:ThreadManager
+ * @Description:异步任务管理
  */
-public final class ThreadManager1 {
+public final class ThreadManager {
 
-    // UI线程
-    private static Handler mManinHandler;
+    /**
+     * UI线程
+     */
+    private static Handler mMainHandler;
     private static Object mMainHandlerLock = new Object();
-
-    // 本地改就行..不要传svn
-    public static final boolean DEBUG_THREAD = false;
 
     /**
      * AsyncTask的默认线程池Executor. 负责长时间的任务(网络访问) 默认3个线程
@@ -51,12 +51,12 @@ public final class ThreadManager1 {
      * 请使用FILE_THREAD_HANDLER 此线程禁止进行网络操作.如果需要进行网络操作. 请使用NETWORK_EXECUTOR
      */
     private static Handler SUB_THREAD_HANDLER;
-
     private static HandlerThread SUB_THREAD;
 
     /**
-     * 文件读写线程的Handle, 只有一个线程 可以执行文件读写操作, 如图片解码等 此线程禁止进行网络操作.如果需要进行网络操作.
-     * 请使用NETWORK_EXECUTOR
+     * 文件读写线程的Handle, 只有一个线程 可以执行文件读写操作,
+     * 如图片解码等，此线程禁止进行网络操作.
+     * 如果需要进行网络操作，请使用NETWORK_EXECUTOR.
      */
     private static Handler FILE_THREAD_HANDLER;
     /**
@@ -64,34 +64,17 @@ public final class ThreadManager1 {
      */
     private static HandlerThread FILE_THREAD;
 
+    /**
+     * 初始化线程池
+     */
     static {
         NETWORK_EXECUTOR = initNetworkExecutor();
     }
 
     private static Executor initNetworkExecutor() {
-        Executor result = null;
-
-
-        if (Build.VERSION.SDK_INT >= 11) {// 3.0以上
-            //result = AsyncTask.THREAD_POOL_EXECUTOR;
-            result = new ThreadPoolExecutor(1, 1, 0, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
-        }else {// 3.0以下, 反射获取
-            Executor tmp = null;
-            try {
-                Field field = AsyncTask.class.getDeclaredField("sExecutor");
-                field.setAccessible(true);
-                tmp = (Executor) field.get(null);
-            } catch (Exception e) {
-                tmp = new ThreadPoolExecutor(1, 1, 0, TimeUnit.SECONDS,	new LinkedBlockingQueue<Runnable>());
-            }
-            result = tmp;
-        }
-
-        if (result instanceof ThreadPoolExecutor) {
-            // core size减少为3个
-            ((ThreadPoolExecutor) result).setCorePoolSize(3);
-        }
-
+        Executor result = new ThreadPoolExecutor(1, 1,
+                0, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+        ((ThreadPoolExecutor) result).setCorePoolSize(3);
         return result;
     }
 
@@ -105,18 +88,19 @@ public final class ThreadManager1 {
      * @return
      */
     public static Handler getMainHandler() {
-        if (mManinHandler == null) {
+        if (mMainHandler == null) {
             synchronized (mMainHandlerLock) {
-                if (mManinHandler == null) {
-                    mManinHandler = new Handler(Looper.getMainLooper());
+                if (mMainHandler == null) {
+                    mMainHandler = new Handler(Looper.getMainLooper());
                 }
             }
         }
-        return mManinHandler;
+        return mMainHandler;
     }
 
     /**
-     * 在网络线程上执行异步操作. 该线程池负责网络请求等操作 长时间的执行(如网络请求使用此方法执行) 当然也可以执行其他 线程和AsyncTask公用
+     * 在网络线程上执行异步操作. 该线程池负责网络请求等操作
+     * 长时间的执行(如网络请求使用此方法执行) 当然也可以执行其他 线程和AsyncTask公用
      *
      * @param run
      */
@@ -124,6 +108,7 @@ public final class ThreadManager1 {
         try {
             NETWORK_EXECUTOR.execute(run);
         } catch (RejectedExecutionException e) {
+            e.printStackTrace();
         }
     }
 
@@ -137,7 +122,7 @@ public final class ThreadManager1 {
     public static Handler getFileThreadHandler() {
         if (FILE_THREAD_HANDLER == null) {
             synchronized (ThreadManager.class) {
-                FILE_THREAD = new HandlerThread("QQ_FILE_RW");
+                FILE_THREAD = new HandlerThread("APP_FILE_RW");
                 FILE_THREAD.start();
                 FILE_THREAD_HANDLER = new Handler(FILE_THREAD.getLooper());
             }
@@ -167,7 +152,7 @@ public final class ThreadManager1 {
     public static Handler getSubThreadHandler() {
         if (SUB_THREAD_HANDLER == null) {
             synchronized (ThreadManager.class) {
-                SUB_THREAD = new HandlerThread("QQ_SUB");
+                SUB_THREAD = new HandlerThread("APP_SUB");
                 SUB_THREAD.start();
                 SUB_THREAD_HANDLER = new Handler(SUB_THREAD.getLooper());
             }
